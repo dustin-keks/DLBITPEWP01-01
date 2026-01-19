15a21a4055d75de2844a9bbd683b5cac3a1bd940
Dustin Vetter <dustinvetter@icloud.com>
2026-01-19 22:51:35 +0100

    add withFetch() to provideHttpClient()

--- angular-ecommerce/src/app/app-module.ts
+++ angular-ecommerce/src/app/app-module.ts
@@ -4,7 +4,7 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { ProductList } from './components/product-list/product-list';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import { ProductService } from './services/product-service';
 
 @NgModule({
@@ -19,7 +19,7 @@ import { ProductService } from './services/product-service';
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withInterceptorsFromDi()),
+    provideHttpClient(withInterceptorsFromDi(), withFetch()),
     ProductService
   ],
   bootstrap: [App]
