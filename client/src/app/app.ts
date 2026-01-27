2faaf282591b35a85931b2991fb3ec33a9b924d9
Dustin Vetter <dustinvetter@icloud.com>
2026-01-27 21:10:50 +0100

    replace hard coded links with menu component

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { ProductCategoryMenu } from './components/product-category-menu/product-category-menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, ProductCategoryMenu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- client/src/app/components/product-category-menu/product-category-menu.ts
+++ client/src/app/components/product-category-menu/product-category-menu.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ProductCategory } from '../../common/product-category';
 import { ProductService } from '../../services/product-service';
 
 @Component({
   selector: 'app-product-category-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './product-category-menu.html',
   styleUrl: './product-category-menu.css',
 })
@@ -20,7 +21,7 @@ export class ProductCategoryMenu {
   listProductCategories() {
     this.productService.getProductCategories().subscribe(
       data => {
-        console.log('Product Categories = ' +JSON.stringify(data));
+        // console.log('Product Categories = ' + JSON.stringify(data));
         this.productCategories = data;
       }
     );
