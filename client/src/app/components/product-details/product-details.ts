cf9480ad8a3a5b427f372859d07ff2892a153b8c
Dustin Vetter <dustinvetter@icloud.com>
2026-02-04 21:54:06 +0100

    Product master-detail-view add data to ProductDetails

--- client/src/app/components/product-details/product-details.ts
+++ client/src/app/components/product-details/product-details.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../common/product';
+import { ProductService } from '../../services/product-service';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [CurrencyPipe, RouterLink],
   templateUrl: './product-details.html',
   styleUrl: './product-details.css',
 })
-export class ProductDetails {
+export class ProductDetails implements OnInit{
+  product!: Product;
 
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    })
+  }
+
+  handleProductDetails() {
+    // get the "id" param string
+    // convert string to number using the "+" symbol
+    const theProductId: number = +this.route.snapshot.paramMap.get('id')!;
+
+    this.productService.getProduct(theProductId).subscribe(
+      data => {
+        this.product = data;
+      }
+    )
+  }
 }
