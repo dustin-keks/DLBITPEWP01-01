9206f2ec05c921d72d8491931ee54e35071c9a6d
Dustin Vetter <dustinvetter@icloud.com>
2026-02-06 15:36:56 +0100

    update ProductList for pagination support

--- client/src/app/components/product-list/product-list.ts
+++ client/src/app/components/product-list/product-list.ts
@@ -14,7 +14,13 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 export class ProductList implements OnInit {
   products: Product[] = [];
   currentCategoryId: number = 1;
+  previousCategoryId: number = 1;
   searchMode: boolean = false;
+
+  // new properties for pagination
+  thePageNumber: number = 1;
+  thePageSize: number = 10;
+  theTotalElements: number = 0;
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) {
@@ -61,11 +67,31 @@ export class ProductList implements OnInit {
       this.currentCategoryId = 1;
     }
 
+    //
+    // Check if we have a different category than previous
+    // Note: Angular will reuse a component if it is currently being viewed
+    //
+
+    // if we have a different category id than the previous
+    // then set thePageNumber back to 1
+    if (this.previousCategoryId != this.currentCategoryId) {
+      this.thePageNumber = 1;
+    }
+
+    this.previousCategoryId = this.previousCategoryId;
+
+    console.log(`currentCategoryId = ${this.currentCategoryId}, thePageNumber = ${this.thePageNumber}`);
+
     // now get the products for the given category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(
+    this.productService.getProductListPaginate(this.thePageNumber - 1, // Spring pages are 0 based - Angular pages are 1 based
+                                              this.thePageSize,
+                                              this.currentCategoryId).subscribe(
       data => {
-        this.products = data;
+        this.products = data._embedded.products;
+        this.thePageNumber = data.page.number + 1; // Spring pages are 0 based - Angular pages are 1 based
+        this.thePageSize = data.page.size;
+        this.theTotalElements = data.page.totalElements;
       }
-    )
+    );
   }
 }
