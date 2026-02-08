0b6500c15f87effe020dbb54e67aa55f88611526
Dustin Vetter <dustinvetter@icloud.com>
2026-02-08 13:57:40 +0100

    shopping cart generate new component

---
+++ client/src/app/components/cart-status/cart-status.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-status',
+  imports: [],
+  templateUrl: './cart-status.html',
+  styleUrl: './cart-status.css',
+})
+export class CartStatus {
+
+}
