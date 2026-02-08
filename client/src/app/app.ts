1db2d85f44c70100abeea7ef1edf5d1df3effb39
Dustin Vetter <dustinvetter@icloud.com>
2026-02-08 15:47:57 +0100

    shopping cart update new component

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductCategoryMenu } from './components/product-category-menu/product-category-menu';
 import { Search } from './components/search/search';
+import { CartStatus } from './components/cart-status/cart-status';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductCategoryMenu, Search],
+  imports: [RouterOutlet, ProductCategoryMenu, Search, CartStatus],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
