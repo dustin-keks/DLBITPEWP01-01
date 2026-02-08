5efa439c05d554f032c73db732c5507220e94fb5
Dustin Vetter <dustinvetter@icloud.com>
2026-02-08 21:34:58 +0100

    shopping cart update CartStatus

--- client/src/app/components/cart-status/cart-status.ts
+++ client/src/app/components/cart-status/cart-status.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/cart-service';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-cart-status',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './cart-status.html',
   styleUrl: './cart-status.css',
 })
-export class CartStatus {
+export class CartStatus implements OnInit {
+  totalPrice: number = 0.00;
+  totalQuantity: number = 0;
 
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.updateCartStatus();
+  }
+
+  updateCartStatus() {
+    // subscribe to the cart totalPrice
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    );
+
+    // subscribe to the cart totalQuantity
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    );
+  }
 }
