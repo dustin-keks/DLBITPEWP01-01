c081e97020e0d966b2944fcf907ee86bf91a1092
Dustin Vetter <dustinvetter@icloud.com>
2026-02-08 13:05:15 +0100

    pagination add select for page size

--- client/src/app/components/product-list/product-list.ts
+++ client/src/app/components/product-list/product-list.ts
@@ -20,7 +20,7 @@ export class ProductList implements OnInit {
 
   // new properties for pagination
   thePageNumber: number = 1;
-  thePageSize: number = 10;
+  thePageSize: number = 5;
   theTotalElements: number = 0;
 
   constructor(private productService: ProductService,
@@ -94,5 +94,11 @@ export class ProductList implements OnInit {
         this.theTotalElements = data.page.totalElements;
       }
     );
+  }
+
+  updatePageSize(pageSize: string) {
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
   }
 }
