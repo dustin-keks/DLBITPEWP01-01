198b8a117133a9f4230ef96f017a30df3c0784ec
Dustin Vetter <dustinvetter@icloud.com>
2026-02-09 19:09:59 +0100

    shopping cart details add routerLink

--- client/src/app/components/cart-status/cart-status.ts
+++ client/src/app/components/cart-status/cart-status.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../../services/cart-service';
 import { CurrencyPipe } from '@angular/common';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-cart-status',
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, RouterLink],
   templateUrl: './cart-status.html',
   styleUrl: './cart-status.css',
 })
