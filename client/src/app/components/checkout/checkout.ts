39d1eade0df490d553325116e6d5f1fa957e764b
Dustin Vetter <dustinvetter@icloud.com>
2026-02-16 20:50:05 +0100

    checkout - drop down lists dependant fields for cc expiration months and years

--- client/src/app/components/checkout/checkout.ts
+++ client/src/app/components/checkout/checkout.ts
@@ -72,7 +72,30 @@ export class Checkout implements OnInit {
   }
 
   onSubmit() {
-    console.log("Handling the submit button");
+    console.log('Handling the submit button');
     console.log(this.checkoutFormGroup.get('customer')?.value);
+  }
+
+  handleMonthsAndYears() {
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup?.value.expirationYear);
+
+    // if the current year equals the selected year, then start with the current month
+    let startMonth: number;
+
+    if (currentYear === selectedYear) {
+      startMonth = new Date().getMonth() + 1; // months are 0-based
+    } else {
+      startMonth = 1;
+    }
+
+    this.checkoutFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log('Retrieved credit card months: ' + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    );
   }
 }
