490ddf5910943bddde0768efd4e5451d08f8f80f
Dustin Vetter <dustinvetter@icloud.com>
2026-03-05 20:09:55 +0100

    login/logout (frontend) - generate component LoginStatus

---
+++ client/src/app/components/login-status/login-status.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-status',
+  imports: [],
+  templateUrl: './login-status.html',
+  styleUrl: './login-status.css',
+})
+export class LoginStatus {
+
+}
