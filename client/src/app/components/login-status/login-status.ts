87e68099da2f7663ecb7111074577f382e05b1ae
Dustin Vetter <dustinvetter@icloud.com>
2026-03-06 16:53:22 +0100

    login/logout (frontend) - add LoginStatus component

--- client/src/app/components/login-status/login-status.ts
+++ client/src/app/components/login-status/login-status.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../services/auth-service';
+import { RouterLink } from '@angular/router';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-login-status',
-  imports: [],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './login-status.html',
   styleUrl: './login-status.css',
 })
-export class LoginStatus {
+export class LoginStatus implements OnInit {
+  isLoggedIn!: Observable<boolean>;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.isLoggedIn();
+  }
 }
