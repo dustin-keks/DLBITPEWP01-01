c4cf5c0a690e4e857181d19be1226492e0da1696
Dustin Vetter <dustinvetter@icloud.com>
2026-03-06 16:55:22 +0100

    login/logout (frontend) - add login-button in header

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 import { ProductCategoryMenu } from './components/product-category-menu/product-category-menu';
 import { Search } from './components/search/search';
 import { CartStatus } from './components/cart-status/cart-status';
+import { LoginStatus } from './components/login-status/login-status';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, ProductCategoryMenu, Search, CartStatus],
+  imports: [RouterOutlet, RouterLink, ProductCategoryMenu, Search, CartStatus, LoginStatus],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
