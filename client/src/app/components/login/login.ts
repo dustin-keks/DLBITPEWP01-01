0c50317c07fc7e483762e8fd57c56ea32ecb9518
Dustin Vetter <dustinvetter@icloud.com>
2026-03-06 16:54:15 +0100

    login/logout (frontend) - add login form

--- client/src/app/components/login/login.ts
+++ client/src/app/components/login/login.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth-service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, AsyncPipe],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {
+export class Login implements OnInit {
+  email = '';
+  password = '';
+  successMessage = '';
+  errorMessage = '';
 
+  isLoggedIn!: Observable<boolean>;
+  userEmail!: Observable<string>;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.isLoggedIn();
+    this.userEmail = this.authService.getUserEmail();
+  }
+
+  onSubmit(): void {
+    this.errorMessage = '';
+    this.authService.login(this.email, this.password).subscribe({
+      next: () => this.successMessage = 'Login erfolgreich!',
+      error: () => this.errorMessage = 'Die E-Mail-Adresse oder das Passwort sind falsch'
+    });
+  }
+
+  logout(): void {
+    this.authService.logout().subscribe(() => {
+      this.successMessage = 'Du wurdest erfolgreich ausgeloggt';
+    });
+  }
 }
