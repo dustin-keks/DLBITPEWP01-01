537642f6bed5b09c44513c29bb3fc32d2ffe658b
Dustin Vetter <dustinvetter@icloud.com>
2026-03-13 22:24:22 +0100

    admin (backend) - add @JsonIgnore

--- ecommerce/src/main/java/com/example/ecommerce/entity/ProductCategory.java
+++ ecommerce/src/main/java/com/example/ecommerce/entity/ProductCategory.java
@@ -2,6 +2,8 @@ package com.example.ecommerce.entity;
 
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -24,6 +26,7 @@ public class ProductCategory {
     @Column(name = "category_name")
     private String categoryName;
 
+    @JsonIgnore
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
     private Set<Product> products;
 }
