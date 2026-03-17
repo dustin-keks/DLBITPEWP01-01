8cd3269e7001692c896524ec8b9b66936f420e9c
Dustin Vetter <dustinvetter@icloud.com>
2026-03-17 14:12:05 +0100

    admin (frontend) - generate component AdminCategoryList

---
+++ client/src/app/components/admin-category-list/admin-category-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-category-list',
+  imports: [],
+  templateUrl: './admin-category-list.html',
+  styleUrl: './admin-category-list.css',
+})
+export class AdminCategoryList {
+
+}
