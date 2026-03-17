47b319960717db538fec1bc0bef230d2c2849a62
Dustin Vetter <dustinvetter@icloud.com>
2026-03-17 23:11:19 +0100

    admin (frontend) - generate component AdminBanner

---
+++ client/src/app/components/admin-banner/admin-banner.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-banner',
+  imports: [],
+  templateUrl: './admin-banner.html',
+  styleUrl: './admin-banner.css',
+})
+export class AdminBanner {
+
+}
