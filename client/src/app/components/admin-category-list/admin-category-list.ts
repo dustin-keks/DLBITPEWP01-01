617c4052446323452ccd610c26296a6fcbb7cf4a
Dustin Vetter <dustinvetter@icloud.com>
2026-03-18 22:48:56 +0100

    admin (frontend) - add AdminCategoryList component

--- client/src/app/components/admin-category-list/admin-category-list.ts
+++ client/src/app/components/admin-category-list/admin-category-list.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductCategory } from '../../common/product-category';
+import { AdminService } from '../../services/admin-service';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-admin-category-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './admin-category-list.html',
   styleUrl: './admin-category-list.css',
 })
-export class AdminCategoryList {
+export class AdminCategoryList implements OnInit {
+  categories: ProductCategory[] = [];
 
+  constructor(private adminService: AdminService) {}
+
+  ngOnInit(): void {
+    this.loadCategories();
+  }
+
+  loadCategories() {
+    this.adminService.getCategories().subscribe(data => {
+      this.categories = data;
+    });
+  }
+
+  deleteCategory(id: number) {
+    if (confirm('Möchtest du die Kategorie wirklich löschen?')) {
+      this.adminService.deleteCategory(id).subscribe(() => {
+        // re-load list after deleting the category
+        this.loadCategories();
+      })
+    }
+  }
 }
