5d478c7b54ce50d963182524f397cfed59c2820d
Dustin Vetter <dustinvetter@icloud.com>
2026-03-18 22:53:07 +0100

    admin (frontend) - include AdminBanner component in header

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -4,10 +4,11 @@ import { ProductCategoryMenu } from './components/product-category-menu/product-
 import { Search } from './components/search/search';
 import { CartStatus } from './components/cart-status/cart-status';
 import { LoginStatus } from './components/login-status/login-status';
+import { AdminBanner } from './components/admin-banner/admin-banner';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, ProductCategoryMenu, Search, CartStatus, LoginStatus],
+  imports: [RouterOutlet, RouterLink, ProductCategoryMenu, Search, CartStatus, LoginStatus, AdminBanner],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
