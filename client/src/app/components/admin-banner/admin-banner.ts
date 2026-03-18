cfc932dbe2e5b073a84df269cba0081ecd81d289
Dustin Vetter <dustinvetter@icloud.com>
2026-03-18 22:52:12 +0100

    admin (frontend) - add AdminBanner component

--- client/src/app/components/admin-banner/admin-banner.ts
+++ client/src/app/components/admin-banner/admin-banner.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../services/auth-service';
+import { AsyncPipe } from '@angular/common';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-admin-banner',
-  imports: [],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './admin-banner.html',
   styleUrl: './admin-banner.css',
 })
-export class AdminBanner {
+export class AdminBanner implements OnInit {
+  userRole!: Observable<string>;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.userRole = this.authService.getRole();
+  }
 }
