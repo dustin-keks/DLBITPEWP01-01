6c48c7fcb72edbc1b9c2410d0541d24ab6ee2bb7
Dustin Vetter <dustinvetter@icloud.com>
2026-03-18 22:47:42 +0100

    admin (frontend) - add adminGuard for route protection

--- client/src/app/guards/admin-guard.ts
+++ client/src/app/guards/admin-guard.ts
@@ -1,5 +1,19 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth-service';
+import { inject } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 
-export const adminGuard: CanActivateFn = (route, state) => {
-  return true;
+export const adminGuard: CanActivateFn = async (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  // current user role
+  const role = await firstValueFrom(authService.fetchRole());
+
+  if (role === 'ROLE_ADMIN') {
+    return true;
+  } else {
+    router.navigateByUrl('/login');
+    return false;
+  }
 };
