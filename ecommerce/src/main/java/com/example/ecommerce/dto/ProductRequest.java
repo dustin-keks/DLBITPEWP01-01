01694c0a231e3160ca5e6b4401452d4b86c093e4
Dustin Vetter <dustinvetter@icloud.com>
2026-03-19 22:00:40 +0100

    admin (backend) - add ProductRequest DTO for admin poduct endpoints

---
+++ ecommerce/src/main/java/com/example/ecommerce/dto/ProductRequest.java
@@ -0,0 +1,19 @@
+package com.example.ecommerce.dto;
+
+import java.math.BigDecimal;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductRequest {
+    private String sku;
+    private String name;
+    private String description;
+    private BigDecimal unitPrice;
+    private String imageUrl;
+    private boolean active;
+    private int unitsInStock;
+    private Long categoryId;
+}
