8e0295cabf9ec4619eb0da378f1e466ae24de305
Dustin Vetter <dustinvetter@icloud.com>
2026-03-19 16:12:04 +0100

    admin (frontend) - generate component AdminProductList

---
+++ client/src/app/components/admin-product-list/admin-product-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-product-list',
+  imports: [],
+  templateUrl: './admin-product-list.html',
+  styleUrl: './admin-product-list.css',
+})
+export class AdminProductList {
+
+}
