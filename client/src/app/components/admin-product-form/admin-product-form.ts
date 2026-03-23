db88ce7290a77951d66c2582c53e8dbf848fc5e7
Dustin Vetter <dustinvetter@icloud.com>
2026-03-23 20:26:20 +0100

    admin (frontend) - add AdminProductForm component

--- client/src/app/components/admin-product-form/admin-product-form.ts
+++ client/src/app/components/admin-product-form/admin-product-form.ts
@@ -1,11 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductCategory } from '../../common/product-category';
+import { AdminService } from '../../services/admin-service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-product-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './admin-product-form.html',
   styleUrl: './admin-product-form.css',
 })
-export class AdminProductForm {
+export class AdminProductForm implements OnInit {
+  // form data ... matches the ProductRequest structure in the backend
+  product = {
+    sku: '',
+    name: '',
+    description: '',
+    unitPrice: 0,
+    imageUrl: '',
+    active: true,
+    unitsInStock: 0,
+    categoryId: 0
+  }
 
+  // categories for the dropdown list
+  categories: ProductCategory[] = [];
+
+  isEditMode = false;
+  productId = 0;
+
+  constructor(
+    private adminService: AdminService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    // load all categories for the dropdown list
+    this.adminService.getCategories().subscribe(data => {
+      this.categories = data;
+    });
+
+    // check if URL contains ID for edit mode
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id) {
+      this.isEditMode = true;
+      this.productId = +id;
+
+      // load product data and map to form object
+      this.adminService.getProduct(this.productId).subscribe((data: any) => {
+        this.product = {
+          sku: data.sku,
+          name: data.name,
+          description: data.description,
+          unitPrice: data.unitPrice,
+          imageUrl: data.imageUrl,
+          active: data.active,
+          unitsInStock: data.unitsInStock,
+          categoryId: data.category.id ?? 0
+        }
+      })
+    }
+  }
+
+  onSubmit() {
+    if (this.isEditMode) {
+      // update product
+      this.adminService.updateProduct(this.productId, this.product).subscribe(() => {
+        this.router.navigateByUrl('/admin/products');
+      })
+    } else {
+      // create product
+      this.adminService.createProduct(this.product).subscribe(() => {
+        this.router.navigateByUrl('/admin/products');
+      })
+    }
+  }
 }
