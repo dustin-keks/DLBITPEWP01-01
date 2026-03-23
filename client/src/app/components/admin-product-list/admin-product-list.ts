ec6baad83f6293bc3588168978f11bf531ef321f
Dustin Vetter <dustinvetter@icloud.com>
2026-03-23 20:25:48 +0100

    admin (frontend) - add AdminProductList component

--- client/src/app/components/admin-product-list/admin-product-list.ts
+++ client/src/app/components/admin-product-list/admin-product-list.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../../services/admin-service';
+import { Product } from '../../common/product';
+import { CurrencyPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-admin-product-list',
-  imports: [],
+  imports: [CurrencyPipe, RouterLink],
   templateUrl: './admin-product-list.html',
   styleUrl: './admin-product-list.css',
 })
-export class AdminProductList {
+export class AdminProductList implements OnInit {
+  products: Product[] = [];
 
+  constructor(private adminService: AdminService) {}
+  
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts() {
+    this.adminService.getProducts().subscribe(data => {
+      this.products = data;
+    });
+  }
+
+  deleteProduct(id: number) {
+    if (confirm('Möchtest du das Produkt wirklich löschen?')) {
+      this.adminService.deleteProduct(id).subscribe(() => {
+        // re-load list after deleting the product
+        this.loadProducts();
+      })
+    }
+  }
 }
