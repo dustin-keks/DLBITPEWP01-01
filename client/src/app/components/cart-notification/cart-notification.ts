c906184db809e85e6e2b3dd04a0b85b838588850
Dustin Vetter <dustinvetter@icloud.com>
2026-03-28 21:55:23 +0100

    cart notification - generate new component

---
+++ client/src/app/components/cart-notification/cart-notification.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-notification',
+  imports: [],
+  templateUrl: './cart-notification.html',
+  styleUrl: './cart-notification.css',
+})
+export class CartNotification {
+
+}
